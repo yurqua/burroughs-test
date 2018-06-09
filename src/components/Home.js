8a7188a4fe313d8438a242c608c3d7f23b11897c
Iurii Zharkov <yurqua@gmail.com>
2018-06-10 00:14:11 +0300

    + Styled Resources block + Added svg based inner shadow header

--- src/components/Home.js
+++ src/components/Home.js
@@ -95,50 +95,77 @@ class Home extends Component {
             <Container>
               <content>
                 <Row className='resources'>
-                  <Col xs={12}>
-                    <h2>Resources</h2>
+                  <Col xs={12} className='header'>
+                    <h2>
+                      <span>Resources</span>
+                      <svg width={360} height={75} viewBox={[0, 0, 360, 75]} style={{'shapeRendering': 'geometricPrecision'}}>
+                        <rect x={0} y={0} width={360} height={70} style={{fill: 'transparent'}}/>
+                        <g style={{'overflow':'hidden', 'textAnchor': 'middle', 'fontSize':'65', 'fontWeight': 'bold'}}>
+                          <defs>
+                            <mask id="textMask">
+                              <text style={{'fill':'white'}} x="175" y="55">Resources</text>
+                            </mask>
+                            <filter id="innerShadow" x="-20%" y="-20%" width="140%" height="140%">
+                              <feGaussianBlur in="SourceGraphic" stdDeviation="1.5" result="blur"/>
+                              <feOffset in="blur" dx="1.5" dy="1.5"/>
+                            </filter>
+                          </defs>
+                          <g mask={'url(#textMask)'}>
+                            <rect x="0" y="0" width="360" height="75" style={{'fill':'#555'}}/>
+                            <text style={{
+                              fill: '#ffd41c', 
+                              'filter': 'url(#innerShadow)'
+                            }} x="175" y="55">Resources</text>
+                          </g>
+                        </g>
+                      </svg>
+                    </h2>
                   </Col>
-                  <Col xs={12} md={4} className='article'>
-                    <h3>White papers</h3>
-                    <div className='content'>
-                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                      <p>Fusce tortor arcu, accumsan in turpis et, efficitur porttitor ligula.</p>
-                      <p>Morbi ac gravida diam.</p>
-                    </div>
-                    <div className='more-link'>
-                      <Link to='/'>More</Link> White Papers 
-                    </div>
-                  </Col>
-                  <Col xs={12} md={3} className='article'>
-                    <h3>Resources</h3>
-                    <div className='content'>
-                      <p>
-                        <Link to='/'>Click here</Link> to enter your email address and receive our Monthly Newsletter
-                      </p>
-                    </div>
-                    <div className='more-link'>
-                      <Link to='/'>More</Link> Resources 
-                    </div>
-                  </Col>
-                  <Col xs={12} md={5} className='article'>
-                    <h3>Inviqa techportal</h3>
-                    <div className='content'>
-                      <Row>
-                        <Col xs={12} sm={6}>
+                  <Col xs={12} className='article-block'>
+                    <Row>
+                      <Col xs={12} md={4} className='article'>
+                        <h3>White papers</h3>
+                        <div className='content'>
+                          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                          <p>Fusce tortor arcu, accumsan in turpis et, efficitur porttitor ligula.</p>
+                          <p>Morbi ac gravida diam.</p>
+                        </div>
+                        <div className='more-link'>
+                          <Link to='/'>More</Link> White Papers 
+                        </div>
+                      </Col>
+                      <Col xs={12} md={3} className='article'>
+                        <h3>Resources</h3>
+                        <div className='content'>
                           <p>
-                            The Inviqa techPortal is an information base for everything related to 
-                            PHP – from developers, to developers.
+                            <Link to='/'>Click here</Link> to enter your email address and receive our Monthly Newsletter
                           </p>
-                        </Col>
-                        <Col xs={12} sm={6}>
-                          <Link to='/'>Image</Link>
-                          <p>Visit our techPortal</p>
-                        </Col>
-                      </Row>
-                    </div>
-                    <div className='more-link'>
-                      <Link to='/'>View</Link> the latest vacancies 
-                    </div>
+                        </div>
+                        <div className='more-link'>
+                          <Link to='/'>More</Link> Resources 
+                        </div>
+                      </Col>
+                      <Col xs={12} md={5} className='article'>
+                        <h3>Inviqa techportal</h3>
+                        <div className='content'>
+                          <Row>
+                            <Col xs={12} sm={6}>
+                              <p>
+                                The Inviqa techPortal is an information base for everything related to 
+                                PHP – from developers, to developers.
+                              </p>
+                            </Col>
+                            <Col xs={12} sm={6}>
+                              <Link to='/'>Image</Link>
+                              <p>Visit our techPortal</p>
+                            </Col>
+                          </Row>
+                        </div>
+                        <div className='more-link'>
+                          <Link to='/'>View</Link> the latest vacancies 
+                        </div>
+                      </Col>
+                    </Row>
                   </Col>
                 </Row>
               </content>
@@ -150,7 +177,6 @@ class Home extends Component {
                 </content>
               </Container>
             </Container>
-  
         </div>
       );
     }
